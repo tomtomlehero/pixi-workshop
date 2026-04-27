7b4a6edf3417e0a5f0197320932e652e373a1137
mathieu <mathieu@live.co.uk>
2026-04-27 13:39:26 +0200

    Application initialisation

--- src/main.js
+++ src/main.js
@@ -1,2 +1,18 @@
+import {Application} from "pixi.js";
+
 (async() => {
+
+    const app = new Application();
+    await app.init({
+        // width: 800,
+        // height: 600,
+        // width: window.innerWidth,
+        // height: window.innerHeight,
+        resizeTo: window,
+        backgroundColor: 0x00ff,
+        backgroundAlpha: 0.8,
+    });
+    // app.canvas.style.position = "absolute";
+    document.body.appendChild(app.canvas);
+
 })();
