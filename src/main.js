2139b7946acca56ed000f110447ee7f1666a9b66
mathieu <mathieu@live.co.uk>
2026-04-27 13:47:09 +0200

    Add basic shape

--- src/main.js
+++ src/main.js
@@ -1,4 +1,4 @@
-import {Application} from "pixi.js";
+import {Application, Graphics} from "pixi.js";
 
 (async() => {
 
@@ -9,10 +9,22 @@ import {Application} from "pixi.js";
         // width: window.innerWidth,
         // height: window.innerHeight,
         resizeTo: window,
-        backgroundColor: 0x00ff,
-        backgroundAlpha: 0.8,
+        backgroundColor: 0x000000,
+        backgroundAlpha: 0.9,
+        // antialias: true,
     });
     // app.canvas.style.position = "absolute";
     document.body.appendChild(app.canvas);
+
+    const c = new Graphics()
+        .circle(200, 200, 50)
+        .fill({
+            color: 0x0000ff,
+            alpha: 0.7})
+        .stroke({
+            width: 6,
+            color: 0xff0000,
+            alpha: 0.7});
+    app.stage.addChild(c);
 
 })();
